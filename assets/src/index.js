398cd293c1bbe09b9701fe3da5aef758d92870a0
Mohammed Sanusi <macbook@Macbooks-MacBook-Pro.local>
2019-04-15 08:47:15 +0100

    fresh worry

---
+++ assets/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return (
+    <div>
+      Bonjour Mon Ami, Ifeoluwa
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
